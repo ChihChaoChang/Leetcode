d2c96ae2d4b10a1a13fb18c4ce9ad62fc2b30c9a
Chih-Chao Chang <cchan113@jhu.edu>
2017-01-29 23:11:53 -0500

    Update 448.py

--- 448.py
+++ 448.py
@@ -28,3 +28,10 @@ class Solution(object):
                 if i not in numSe:
                     list.append(i)
             return list
+'''
+better solution:
+for n in nums:
+            nums[abs(n) - 1] = -abs(nums[abs(n) - 1])
+        return [i + 1 for i, n in enumerate(nums) if n > 0]
+
+'''
