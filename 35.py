7fb60984f3514e72d0edcee658e9254eab579826
Chih-Chao Chang <cchan113@jhu.edu>
2017-03-07 16:52:27 -0500

    Update 35.py

--- 35.py
+++ 35.py
@@ -33,3 +33,24 @@ class Solution(object):
 		            return (i)
             i+=1
         return i
+'''
+Better way to do it: By using binary search
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0; right = len(nums) - 1
+        while left <= right:
+            mid = ( left + right ) / 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return left
+'''
