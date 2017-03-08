f4c96cbef0bd526ef4bb050e9bbc9c254e791c0d
Chih-Chao Chang <cchan113@jhu.edu>
2017-03-08 12:39:03 -0500

    Create 441.py

---
+++ 441.py
@@ -0,0 +1,54 @@
+'''
+Arranging Coins
+
+You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+Given n, find the total number of full staircase rows that can be formed.
+
+n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+
+'''
+
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        i=0
+        num=n
+        counter=0
+        while num>0 and i< n :
+	        i+=1
+	        num=num-i
+	        counter+=1
+        if num <0:
+            return (i-1)
+        elif num ==0:
+            return (counter)
+'''
+Better way to do it:
+
+'''
