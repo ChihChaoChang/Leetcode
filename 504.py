478a4d911ad70effb290a2bb55f5f04678557255
Chih-Chao Chang <cchan113@jhu.edu>
2017-03-27 22:51:08 -0400

    Create 504.py

---
+++ 504.py
@@ -0,0 +1,33 @@
+'''
+Base 7
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+
+Example 2:
+Input: -7
+Output: "-10"
+
+Note: The input will be in range of [-1e7, 1e7].
+'''
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        string=""
+        if num<0:
+            sign="-"
+        else:
+            sign=""
+        num=abs(num)
+        while num>=7:
+            string+= str(num%7)
+            num/=7
+        
+        if num>=0:
+            new_string=string+str(num)
+            return sign+new_string[::-1]
