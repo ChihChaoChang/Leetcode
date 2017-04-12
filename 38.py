d679da2e81a85c19f273c18d0a91b1b64e59e098
Chih-Chao Chang <cchan113@jhu.edu>
2017-04-12 13:40:28 -0400

    Create 38.py

---
+++ 38.py
@@ -0,0 +1,20 @@
+'''
+
+The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string. 
+
+
+'''
+  #When n = 1, print 1
+  #When n = 2, print 11
+  #When n = 3, print 21
+  #When n = 4, print 1211
+  # ** The n starts from 1
