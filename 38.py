d5cb994921c813b626317334abf3357377a9e55b
Chih-Chao Chang <cchan113@jhu.edu>
2017-04-12 14:33:40 -0400

    Update 38.py

--- 38.py
+++ 38.py
@@ -18,3 +18,28 @@ Note: The sequence of integers will be represented as a string.
   #When n = 3, print 21
   #When n = 4, print 1211
   # ** The n starts from 1
+  class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        s = '1'
+        for i in range(2,n+1):
+            s=self.count(s)
+        return s
+        
+    def count(self,s):
+        t=""
+        count = 0
+        curr = "*"
+        for i in s:
+            if i != curr:
+                if curr != '*':
+                    t += str(count)+curr
+                curr = i
+                count = 1
+            else:
+                count+=1
+        t += str(count)+curr
+        return t
