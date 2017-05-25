4990877c8aa8f0058acd04602cab3cfe6c4598b7
Chih-Chao Chang <cchan113@jhu.edu>
2017-05-25 16:43:39 -0400

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,31 @@
+'''
+Divide Two Integers 
+
+ Divide two integers without using multiplication, division and mod operator.
+
+If it is overflow, return MAX_INT. 
+
+'''
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        if (dividend >0 and divisor >0) or (dividend <0 and divisor <0):
+            sign =1
+        else:
+            sign =-1
+        q=0
+        dividend=abs(dividend)
+        divisor =abs(divisor)
+        while dividend >= divisor:
+            k=0 
+            tmp=divisor
+            while dividend >= tmp:
+                q+= 1<< k
+                dividend -=tmp
+                tmp <<=1
+                k+=1
+        return q*sign
